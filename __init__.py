95aaea78c6f0b6e95b6f2b2566c76fefd085febf
Sebastian Monroy <sebastian.monroy@gmail.com>
2025-08-04 14:24:38 +0100

    removed obsolete image_stream code

--- __init__.py
+++ __init__.py
@@ -14,19 +14,9 @@ consumer_thread = threading.Thread(target=consume_jobs, daemon=True)
 consumer_thread.start()
 print("✅ Nilor-Nodes: SQS worker consumer thread started.")
 
-# 2. Start the Image Stream FastAPI Server
-from .image_stream import start_server as start_image_stream_server
-start_image_stream_server()
-print("✅ Nilor-Nodes: Image stream server started.")
-
-
 # --- Node Registration ---
 from .nilornodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-from .image_stream import NODE_CLASS_MAPPINGS as is_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as is_NODE_DISPLAY_NAME_MAPPINGS
 from .media_stream import NODE_CLASS_MAPPINGS as ms_NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as ms_NODE_DISPLAY_NAME_MAPPINGS
-
-NODE_CLASS_MAPPINGS.update(is_NODE_CLASS_MAPPINGS)
-NODE_DISPLAY_NAME_MAPPINGS.update(is_NODE_DISPLAY_NAME_MAPPINGS)
 
 NODE_CLASS_MAPPINGS.update(ms_NODE_CLASS_MAPPINGS)
 NODE_DISPLAY_NAME_MAPPINGS.update(ms_NODE_DISPLAY_NAME_MAPPINGS)
